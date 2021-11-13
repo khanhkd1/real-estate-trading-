370ac1c7b72632568642673262c558dd0a5472c7
Kiều Duy Khánh <khanhkieu167@gmail.com>
2021-11-13 11:07:47 +0700

    push data

---
+++ data-to-db/main.py
@@ -0,0 +1,48 @@
+import json
+from model import connect_to_db, Post, Image
+import random
+import time
+
+Session = connect_to_db()
+
+
+def add_post(record, session_):
+    post = Post(
+        user_id=1,
+        title=record['title'],
+        address=record['address'],
+        bedroom=int(record['bedroom']),
+        toilet=int(record['toilet']),
+        investor=record['investor'],
+        acreage=record['acreage'],
+        price=record['price'],
+        latitude=record['lat'],
+        longitude=record['long'],
+        sold=bool(random.getrandbits(1)),
+        time_upload=time.time(),
+        time_priority=time.time(),
+        distance=record['distance'],
+        description=record['description']
+    )
+    session_.add(post)
+    session_.flush()
+
+    for image_ in record['images']:
+        image = Image(
+            post_id=post.post_id,
+            image_url=image_
+        )
+        session_.add(image)
+
+    session_.commit()
+
+
+if __name__ == '__main__':
+    session = Session()
+    with open('/Users/khnhkd/WorkSpace/python_proj/real-estate-trading-/data-process/data-to-db.json') as js_file:
+        data = json.load(js_file)
+
+    for i, record in enumerate(data):
+        add_post(record, session)
+        print(f'done: {i}')
+        break
---
+++ data-to-db/model.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import *
+from sqlalchemy import *
+
+Base = declarative_base()
+
+
+# khoi tao ket noi vao co so du lieu
+def connect_to_db():
+    engine_ = create_engine('mysql+mysqldb://root:vjpvjp123A01@localhost/real-estate-trading?charset=utf8mb4')
+    session_ = sessionmaker(bind=engine_)
+    return session_
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    address = Column(String)
+    price = Column(Float)
+    acreage = Column(String)
+    investor = Column(String)
+    bedroom = Column(Integer)
+    toilet = Column(Integer)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    distance = Column(Float)
+    time_upload = Column(DateTime)
+    time_priority = Column(DateTime)
+    sold = Column(Boolean)
+    description = Column(String)
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    image_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    image_url = Column(String)
