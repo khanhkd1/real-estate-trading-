01bd3649d3c0498779fe5f6b2217332b7b5806fc
Kiều Duy Khánh <khanhkieu167@gmail.com>
2021-11-29 15:54:33 +0700

    limit type file allowed upload

--- api-service/resources/image.py
+++ api-service/resources/image.py
@@ -12,15 +12,20 @@ class ImageApi(Resource):
     def post(self):
         img_urls = []
         for key in request.files.keys():
-            try:
-                img_urls.append(requests.post(
-                    'https://api.imgur.com/3/image',
-                    headers={'Authorization': f'Bearer {IMGUR_TOKEN}'},
-                    data={
-                        'image': b64encode(request.files[key].read()),
-                        'type': 'base64'
-                    }
-                ).json()['data']['link'])
-            except RequestException as e:
-                pass
-        return img_urls
+            if allowed_file(request.files[key].filename):
+                try:
+                    img_urls.append(requests.post(
+                        'https://api.imgur.com/3/image',
+                        headers={'Authorization': f'Bearer {IMGUR_TOKEN}'},
+                        data={
+                            'image': b64encode(request.files[key].read()),
+                            'type': 'base64'
+                        }
+                    ).json()['data']['link'])
+                except RequestException:
+                    continue
+        return {'images': img_urls}, 200
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
