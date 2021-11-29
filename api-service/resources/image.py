0986b9ddcc7abcf06c825461c061265e639e7d2f
Kiều Duy Khánh <khanhkieu167@gmail.com>
2021-11-29 16:25:08 +0700

    update file size max

--- api-service/resources/image.py
+++ api-service/resources/image.py
@@ -4,7 +4,7 @@ import requests
 from requests.exceptions import RequestException
 from base64 import b64encode
 from flask_jwt_extended import jwt_required
-from config import IMGUR_TOKEN
+from config import IMGUR_TOKEN, ALLOWED_FILES, SIZE_FILE_MAX
 
 
 class ImageApi(Resource):
@@ -13,19 +13,21 @@ class ImageApi(Resource):
         img_urls = []
         for key in request.files.keys():
             if allowed_file(request.files[key].filename):
-                try:
-                    img_urls.append(requests.post(
-                        'https://api.imgur.com/3/image',
-                        headers={'Authorization': f'Bearer {IMGUR_TOKEN}'},
-                        data={
-                            'image': b64encode(request.files[key].read()),
-                            'type': 'base64'
-                        }
-                    ).json()['data']['link'])
-                except RequestException:
-                    continue
+                file = request.files[key].read()
+                if len(file) <= SIZE_FILE_MAX:
+                    try:
+                        img_urls.append(requests.post(
+                            'https://api.imgur.com/3/image',
+                            headers={'Authorization': f'Bearer {IMGUR_TOKEN}'},
+                            data={
+                                'image': b64encode(file),
+                                'type': 'base64'
+                            }
+                        ).json()['data']['link'])
+                    except RequestException:
+                        continue
         return {'images': img_urls}, 200
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_FILES
