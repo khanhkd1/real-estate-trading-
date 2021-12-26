f2f3c0e7903827a183579ab1486b445b47516a9d
Kiều Duy Khánh <khanhkieu167@gmail.com>
2021-12-26 15:40:17 +0700

    update cors

--- api-service/app.py
+++ api-service/app.py
@@ -3,19 +3,13 @@ from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from database.db import initialize_db, db
-from database.models import User, Post, Image
+from flask_cors import CORS
+from database.db import initialize_db
 
 app = Flask(__name__)
+CORS(app)
 
 app.config.from_pyfile('config.py')
-
-# admin = Admin(app)
-# admin.add_view(ModelView(User, db.session))
-# admin.add_view(ModelView(Post, db.session))
-# admin.add_view(ModelView(Image, db.session))
 
 mail = Mail(app)
 
